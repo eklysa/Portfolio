e46047fad2da789afa647c56fe107c992c298136
Sylke <heraldingbirds@gmail.com>
2023-10-14 19:53:35 +0200

    working contact page

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+// get dotenv info
+require('dotenv').config();
+
 // variables
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 const Post = require('./models/posts');
+
 
 // get the database
 const DB_URL = 'mongodb://localhost:27017/heraldingbirds1';
@@ -31,6 +37,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // make sure public is assigned as style path
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async(req, res, next) => {
     try {
@@ -54,6 +61,42 @@ app.get('/gallery', async(req, res, next) => {
 
 app.get('/contact', (req, res) => {
     res.render('contact.ejs');
+});
+
+app.post('/', async(req, res, next) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message;
+    console.log(message);
+    try {
+        // create email transporter
+        const transporter = nodemailer.createTransport({
+            service: 'hotmail',
+            auth: {
+                user: process.env.my_email,
+                pass: process.env.my_password,
+            },
+        });
+        // create the email
+        const mailOptions = {
+            from: email, // from sender
+            to: process.env.my_email, // to me
+            subject: 'Contact Form Submission from:'+ name,
+            text: JSON.stringify(message), // give message
+        };
+        // send the email
+        transporter.sendMail(mailOptions,  (error, info) => {
+            if (error) { 
+                console.error('Error sending email:', error);
+                res.status(500).send('An error occurred while sending the email.');
+            } else {
+                console.log('Email sent:', info.response);
+                res.send('Thank you for your message!'); // Respond to the client
+            };
+        });
+    } catch(error) {
+        console.log(error);
+    };
 });
 
 app.listen(3000, () => {
