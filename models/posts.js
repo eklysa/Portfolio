e7e20b8f2c7806407a313212af34a48bcff6fd3c
sylkeTUDelft <s.k.angenent@student.tudelft.nl>
2024-03-09 14:45:17 +0100

    remove sharp

--- models/posts.js
+++ models/posts.js
@@ -1,53 +1,6 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
-// const slugify = require('slugify');
-// const sharp = require('sharp');
-// const path = require('path');
-
-// const PostSchema = new Schema({
-//   title: String,
-//   image: String,
-//   thumbnail: String,
-//   // compressed_thumbnail: String,
-//   alt: String,
-//   class: String,
-//   slug: String
-// });
-
-// // Pre-save middleware to generate the slug based on the title
-// PostSchema.pre('save', async function (next) {
-//   this.slug = slugify(this.title, { lower: true });
-
-//   // const thumbnailPath = path.join(__dirname, 'public', this.thumbnail);
-//   const imagePath = path.join(this.image);
-//   // const thumbnailPath = path.join(__dirname, 'public/images/illustrations/thumbnails', `${this.slug}.jpg`);
-
-//   try {
-//     // Compress and resize the thumbnail image
-//     // await sharp(thumbnailPath)
-//     //   .resize({ width: 500 })
-//     //   .toFile(compressedThumbnailPath);
-//     await sharp(imagePath)
-//       .resize({ width: 500 })
-//       .toFile(thumbnailPath);
-//     // Update the post object with the compressed thumbnail path
-//     this.thumbnail = path.relative('public', thumbnailPath);
-//     // console.log(this.compressed_thumbnail);
-//     console.log(this.thumbnail);
-//   } catch (error) {
-//     console.error(error);
-//   }
-
-//   next();
-// });
-
-// module.exports = mongoose.model('Post', PostSchema);
-
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const slugify = require('slugify');
-const sharp = require('sharp');
 const path = require('path');
 
 const PostSchema = new Schema({
